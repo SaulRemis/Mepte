af90b0bdcc06cdbc3a8a4410112c1c0c25888c7c
Adrian Espina Viella <adrian.espina-viella@arcelormittal.com>
2012-02-25 17:41:39 +0100

    le añadi el hilo de adquisicion vacio

---
+++ Meplate/HiloAdquisicion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SpinPlatform.Dispatcher;
+
+namespace Meplate
+{
+    class HiloAdquisicion : SpinThread
+    {
+        public HiloAdquisicion(string name)
+            : base(name)
+        {
+            _MillisecondsToSleep = 0;
+        }
+
+        public override void FunctionToExecuteByThread()
+        {
+            
+        }
+        public override void Initializate()
+        {
+            
+
+        }
+
+    }
+}
