2aa5a96db97d001ff474399375b76d84137fc090
Adrian Espina Viella <adrian.espina-viella@arcelormittal.com>
2012-02-25 17:45:24 +0100

    Creo el hiloProcesamiento Vacio

---
+++ Meplate/HiloProcesamiento.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SpinPlatform.Dispatcher;
+
+namespace Meplate
+{
+    class HiloProcesamiento: SpinThreadEvent
+    {
+        SpinDispatcher _Padre;
+        public HiloProcesamiento(SpinDispatcher padre, string name)
+            : base(name)
+        {
+             _Padre = padre;
+        }
+        public override void FunctionToExecuteByThread()
+        { 
+        }
+        public override void Initializate()
+        { 
+        }
+    }
+}
